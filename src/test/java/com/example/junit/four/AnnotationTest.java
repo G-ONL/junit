22e2896059ea19b4ce9614b61ec057d387875d52
kimjaeyun <je941004@naver.com>
2020-11-27 00:05:49 +0900

    junit 4에서의 실행 순서에 관한 Annotation들 테스트

--- src/test/java/com/example/junit/four/AnnotationTest.java
+++ src/test/java/com/example/junit/four/AnnotationTest.java
@@ -1,88 +1,82 @@
 package com.example.junit.four;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+@Slf4j
 public class AnnotationTest {
 
-  static Object staticObject = new Object();
+  static Object staticObject;
   Object object1;
 
   @BeforeClass
-  public static void beforeClassTest(){
-    System.out.println("BeforeClass");
-    System.out.println("staticObject" + staticObject.hashCode());
+  public static void beforeClassTest() {
+    log.debug("BeforeClass");
+    staticObject = new Object();
+    log.debug("staticObject : " + staticObject.hashCode());
   }
 
   @Before
-  public void beforeTest(){
-    System.out.println("Before");
+  public void beforeTest() {
+    log.debug("Before");
     object1 = new Object();
-    System.out.println("staticObject" + staticObject.hashCode());
-    System.out.println("object" + object1.hashCode());
+    log.debug("object : " + object1.hashCode());
   }
 
   @After
-  public void afterTest(){
-    System.out.println("After");
+  public void afterTest() {
+    log.debug("After");
     object1 = new Object();
-    System.out.println("staticObject" + staticObject.hashCode());
-    System.out.println("object" + object1.hashCode());
+    log.debug("object : " + object1.hashCode());
   }
 
   @AfterClass
-  public static void afterClassTest(){
-    System.out.println("After");
-    System.out.println("staticObject" + staticObject.hashCode());
+  public static void afterClassTest() {
+    log.debug("After");
+    staticObject = new Object();
+    log.debug("staticObject : " + staticObject.hashCode());
   }
 
   @Test
-  public void test1(){
-    System.out.println("test1");
-    object1 = new Object();
-    System.out.println("staticObject" + staticObject.hashCode());
-    System.out.println("object" + object1.hashCode());
+  public void test1() {
+    log.debug("test1");
+    log.debug("staticObject : " + staticObject.hashCode());
+    log.debug("object : " + object1.hashCode());
   }
 
   @Test
-  public void test2(){
-    System.out.println("test2");
-    object1 = new Object();
-    System.out.println("staticObject" + staticObject.hashCode());
-    System.out.println("object" + object1.hashCode());
+  public void test2() {
+    log.debug("test2");
+    log.debug("staticObject : " + staticObject.hashCode());
+    log.debug("object : " + object1.hashCode());
   }
 
   //output
 
-  //BeforeClass
-  //staticObject1579526446
+  //22:59:44.032 [main] DEBUG com.example.junit.four.AnnotationTest - BeforeClass
+  //22:59:44.070 [main] DEBUG com.example.junit.four.AnnotationTest - staticObject : 667447085
 
-  //Before
-  //staticObject1579526446
-  //object93314457
-  //test1
-  //staticObject1579526446
-  //object1796371666
-  //After
-  //staticObject1579526446
-  //object2076287037
+  //22:59:44.078 [main] DEBUG com.example.junit.four.AnnotationTest - Before
+  //22:59:44.079 [main] DEBUG com.example.junit.four.AnnotationTest - object : 689401025
+  //22:59:44.079 [main] DEBUG com.example.junit.four.AnnotationTest - test1
+  //22:59:44.080 [main] DEBUG com.example.junit.four.AnnotationTest - staticObject : 667447085
+  //22:59:44.080 [main] DEBUG com.example.junit.four.AnnotationTest - object : 689401025
+  //22:59:44.081 [main] DEBUG com.example.junit.four.AnnotationTest - After
+  //22:59:44.081 [main] DEBUG com.example.junit.four.AnnotationTest - object : 790067787
 
-  //Before
-  //staticObject1579526446
-  //object1773638882
-  //test2
-  //staticObject1579526446
-  //object1059063940
-  //After
-  //staticObject1579526446
-  //object1295226194
+  //22:59:44.089 [main] DEBUG com.example.junit.four.AnnotationTest - Before
+  //22:59:44.089 [main] DEBUG com.example.junit.four.AnnotationTest - object : 867148091
+  //22:59:44.089 [main] DEBUG com.example.junit.four.AnnotationTest - test2
+  //22:59:44.090 [main] DEBUG com.example.junit.four.AnnotationTest - staticObject : 667447085
+  //22:59:44.090 [main] DEBUG com.example.junit.four.AnnotationTest - object : 867148091
+  //22:59:44.091 [main] DEBUG com.example.junit.four.AnnotationTest - After
+  //22:59:44.091 [main] DEBUG com.example.junit.four.AnnotationTest - object : 2051853139
 
-  //After
-  //staticObject1579526446
+  //22:59:44.092 [main] DEBUG com.example.junit.four.AnnotationTest - After
+  //22:59:44.093 [main] DEBUG com.example.junit.four.AnnotationTest - staticObject : 815674463
 
 }
